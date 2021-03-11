992c0d011351b773ce5a1530b451e22b1f86ce67
Tofarati Farinu <62319828+tman6290@users.noreply.github.com>
2021-03-11 10:28:20 +0100

    Add files via upload
    Just a fun little project to help me understand the declaration and initialisation of variables better.

---
+++ MadLibs.java
@@ -0,0 +1,37 @@
+public class MadLibs {
+  /*
+  This program generates a mad libbed
+  story.
+  Author: Tofa
+  Date: 11/03/2021
+  */
+  	public static void main(String[] args){
+      String name1= "Tofa";
+      String adjective1= "Happy";
+      String adjective2= "Scary";
+      String adjective3= "Strong";
+      String verb1= "Fight";
+      String noun1= "Toy";
+      String noun2= "Lion";
+      String noun3= "Goat";
+      String noun4= "Palace";
+      String noun5= "Pencil";
+      String noun6= "Wings";
+      String name2= "Bunmi";
+      int number= 1600000000;
+      String place1= "Japan";
+
+      
+      
+      
+
+
+      
+      
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
+    }       
+}
