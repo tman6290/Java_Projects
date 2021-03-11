15c9b25bb83ddf078b77bace494fc4935cc24485
Tofarati Farinu <62319828+tman6290@users.noreply.github.com>
2021-03-11 10:02:47 +0100

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    // I will introduce myself then plant a tree.
+    System.out.println("Hey there, I’m Tofa and I’m learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+
+
+
+  }
+  
+}
